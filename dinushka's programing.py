83f7531493bd1fe880cc5753fce3a1988b1ddd19
dinu2010 <136883432+dinu2010@users.noreply.github.com>
2023-06-19 21:57:29 +0530

    Add files via upload

---
+++ dinushka's programing.py
@@ -0,0 +1,11 @@
+Python 3.12.0b2 (tags/v3.12.0b2:e6c0efa, Jun  6 2023, 15:37:48) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: E:/programing/dinushka's programing.py ===============
+hello,world!
+>>> 
+================ RESTART: E:/programing/dinushka's programing.py ===============
+hello,world!
+hello,world!
+what is your name?dinushka
+hello dinushka
